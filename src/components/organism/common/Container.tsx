5a9d96489a3f608990258ccd46f4efbe31b7232c
isaac <ishak.hadj14@gmail.com>
2025-12-13 10:20:38 +0100

    feat(layout/container): add responsive container wrapper with breakpoint-based widths

---
+++ src/components/organism/common/Container.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Container = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={`container max-w-screen-xl !mx-auto px-4 md:px-8 ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
