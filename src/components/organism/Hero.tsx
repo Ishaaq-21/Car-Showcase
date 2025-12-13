6b69b03ce20b04eca07f10f6d1e1e19a906a88b0
isaac <ishak.hadj14@gmail.com>
2025-12-13 10:40:23 +0100

    refactor: improve car image alt description

--- src/components/organism/Hero.tsx
+++ src/components/organism/Hero.tsx
@@ -19,15 +19,15 @@ const Hero = () => {
         </div>
         <div className="relative lg:static flex-1 flex justify-end items-end w-full mt-10 lg:mt-0">
           <div className="image-car-container relative w-full sm:w-3/4 md:w-3/5 h-64 lg:w-[90%] lg:h-100 md:-translate-x-20 lg:-translate-x-10 ">
-            <Image priority src={"/hero.png"} alt="Hero" fill={true} />
-          </div>
-          <div className="absolute w-full lg:w-1/2 h-112.5 lg:h-screen -skew-y-6 -top-10 lg:-top-1/5 -right-1/4 lg:-right-10 -z-10">
             <Image
               priority
-              src="/hero-bg.png"
-              alt="Hero background image"
+              src={"/hero.png"}
+              alt="Silver Toyota Fortuner"
               fill={true}
             />
+          </div>
+          <div className="absolute w-full lg:w-1/2 h-112.5 lg:h-screen -skew-y-6 -top-10 lg:-top-1/5 -right-1/4 lg:-right-10 -z-10">
+            <Image priority src="/hero-bg.png" alt="" fill={true} />
           </div>
         </div>
       </div>
