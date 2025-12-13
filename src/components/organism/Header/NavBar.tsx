0b14487ac79d0fb6071eca94fff527e1ef34d130
isaac <ishak.hadj14@gmail.com>
2025-12-13 10:38:02 +0100

    refactor: wrap the logo image inside a link + use nav element for the parent component

--- src/components/organism/Header/NavBar.tsx
+++ src/components/organism/Header/NavBar.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="flex-between">
-      <Image src={"/logo.svg"} alt="Logo" width={120} height={40} />
+    <nav className="flex-between">
+      <Link href={"/"}>
+        <Image src={"/logo.svg"} alt="Logo" width={120} height={40} />
+      </Link>
       <button className="z-50 px-4 py-2 rounded-full border font-semibold hover-effect bg-white text-primary-blue cursor-pointer">
         Sign-Up
       </button>
-    </div>
+    </nav>
   );
 };
 
