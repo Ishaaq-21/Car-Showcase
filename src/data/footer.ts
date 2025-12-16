595d371fae67abc172921b433a1dd1c1c7a85034
isaac <ishak.hadj14@gmail.com>
2025-12-16 21:45:16 +0100

    feat(footer-links-data): add link names arrays for footer elements

---
+++ src/data/footer.ts
@@ -0,0 +1,18 @@
+export const company: string[] = [
+  "Events",
+  "Blog",
+  "Podcasts",
+  "Invite a friend",
+];
+export const about: string[] = [
+  "How it works",
+  "Featured",
+  "Partnership",
+  "Business Relation",
+];
+export const Socials: string[] = [
+  "Discord",
+  "Facebook",
+  "Twitter",
+  "Instagram",
+];
