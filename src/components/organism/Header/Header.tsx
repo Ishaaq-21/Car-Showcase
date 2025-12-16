59ad30aad283682b498e85c2246ae13b08788482
isaac <ishak.hadj14@gmail.com>
2025-12-16 21:07:00 +0100

    style: add z-50 class

--- src/components/organism/Header/Header.tsx
+++ src/components/organism/Header/Header.tsx
@@ -4,7 +4,7 @@ import Container from "../common/Container";
 
 const Header = () => {
   return (
-    <header className="absolute w-full pt-4">
+    <header className="absolute w-full pt-4 z-50">
       <Container>
         <NavBar />
       </Container>
