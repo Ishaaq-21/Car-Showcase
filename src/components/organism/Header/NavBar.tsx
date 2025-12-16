5e0fba47e70c10ad52f73758213556121782b616
isaac <ishak.hadj14@gmail.com>
2025-12-16 21:48:00 +0100

    refactor: use the reusable logo component instead of custom one

--- src/components/organism/Header/NavBar.tsx
+++ src/components/organism/Header/NavBar.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Logo from "../../atoms/Logo";
 
 const NavBar = () => {
   return (
     <nav className="flex-between">
-      <Link href={"/"}>
-        <Image src={"/logo.svg"} alt="Logo" width={120} height={40} />
-      </Link>
+      <Logo />
       <button className="z-50 px-4 py-2 rounded-full border font-semibold hover-effect bg-white text-primary-blue cursor-pointer">
         Sign-Up
       </button>
