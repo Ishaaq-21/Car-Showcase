5495051b42326a4f8c29649fd7b2804499d8ed78
isaac <ishak.hadj14@gmail.com>
2025-12-17 20:27:56 +0100

    feat(car-catalogue): create the initial car-catalogue component

---
+++ src/components/organism/CarCatalogue/CarCatalogue.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "../../common/Container";
+
+const CarCatalogue = () => {
+  return (
+    <Container>
+      <div>
+        <h1 className="text-5xl font-bold">Car Catalogue</h1>
+        <p className="text-lg mt-2">Explore out cars you might like</p>
+      </div>
+    </Container>
+  );
+};
+
+export default CarCatalogue;
