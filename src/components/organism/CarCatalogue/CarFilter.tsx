5c94f02e4f14ff81cf80f87639deb1c05431dd7d
isaac <ishak.hadj14@gmail.com>
2025-12-17 22:09:44 +0100

    feat(car-filter): add the car filter component

---
+++ src/components/organism/CarCatalogue/CarFilter.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { carManufacturers } from "@/src/data/cars";
+import React from "react";
+import SearchBar from "./SearchBar";
+
+const CarFilter = () => {
+  const handleFormSubmit = () => {};
+  return (
+    <div className="flex justify-between items-center">
+      <SearchBar />
+    </div>
+  );
+};
+
+export default CarFilter;
