5607ccb77a1b60bf528ac18e7a80cfe7a3390fbc
Ishaaq-21 <ishak.hadj14@gmail.com>
2025-12-29 21:37:14 +0100

    required changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body className="relative">
         <Header />
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import CarCatalogue from "../components/organism/CarCatalogue/CarCatalogue";
 import Hero from "../components/organism/Hero";
 
 export default function Home() {
   return (
-    <main className="relative overflow-hidden min-h-screen flex-center">
+    <main className="relative overflow-hidden">
       <Hero />
+      <CarCatalogue />
     </main>
   );
 }
