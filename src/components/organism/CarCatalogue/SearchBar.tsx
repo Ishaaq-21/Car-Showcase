2b1414e5f599635baabf57398c11c0dac1316fac
Ishaaq-21 <ishak.hadj14@gmail.com>
2025-12-30 22:32:41 +0100

    feat(conditional-rendering): add a state for all cars and render elemenets based on that state

--- src/components/organism/CarCatalogue/SearchBar.tsx
+++ src/components/organism/CarCatalogue/SearchBar.tsx
@@ -1,9 +1,10 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SearchManufacturer from "./SearchManufacturer";
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState("");
+  const [allCars, setAllCars] = useState<null | []>(null);
 
   const handleFormSubmit = () => {};
 
@@ -18,6 +19,19 @@ const SearchBar = () => {
           setManuFacturer={setManufacturer}
         />
       </form>
+      {!allCars ? (
+        <div>Please search a car model or maker</div>
+      ) : allCars.length < 1 ? (
+        <p className="text-2xl font-semibold text-red">
+          Oops! there are no available cars in your chosen maker | model
+        </p>
+      ) : (
+        <div>
+          {allCars.map((car, index) => (
+            <p key={index}>{car.model as string}</p>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
