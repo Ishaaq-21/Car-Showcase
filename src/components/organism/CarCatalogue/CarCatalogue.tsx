e57dbd252db5b7946b26d80aa96a3adbb67c1744
Ishaaq-21 <ishak.hadj14@gmail.com>
2025-12-30 10:41:31 +0100

    style: add vertical margin on med screens

--- src/components/organism/CarCatalogue/CarCatalogue.tsx
+++ src/components/organism/CarCatalogue/CarCatalogue.tsx
@@ -3,7 +3,7 @@ import CarFilter from "./CarFilter";
 
 const CarCatalogue = () => {
   return (
-    <Container className="z-50">
+    <Container className="md:my-20 lg:my-0">
       <div className="mb-5">
         <h1 className="text-5xl font-bold">Car Catalogue</h1>
         <p className="text-lg mt-2">Explore out cars you might like</p>
