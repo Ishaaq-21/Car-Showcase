cc44ed6391cf2d1349748a57498969b91817c590
Ishaaq-21 <ishak.hadj14@gmail.com>
2026-01-02 21:00:44 +0100

    style: increase vertical margin of the container + style the p

--- src/components/organism/CarCatalogue/CarCatalogue.tsx
+++ src/components/organism/CarCatalogue/CarCatalogue.tsx
@@ -3,10 +3,12 @@ import CarFilter from "./CarFilter";
 
 const CarCatalogue = () => {
   return (
-    <Container className="md:my-20 lg:my-0">
+    <Container className="my-30 lg:my-10">
       <div className="mb-5">
         <h1 className="text-5xl font-bold">Car Catalogue</h1>
-        <p className="text-lg mt-2">Explore out cars you might like</p>
+        <p className="text-lg mt-2 font-semibold text-gray-500">
+          Explore out cars you might like
+        </p>
       </div>
       <CarFilter />
     </Container>
